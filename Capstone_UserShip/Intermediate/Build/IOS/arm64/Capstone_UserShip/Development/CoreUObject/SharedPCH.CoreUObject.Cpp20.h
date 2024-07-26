dbbec4d88bed4c170fe5bf63f34a1660a8b1887c
gallivas <102565784+gallivas@users.noreply.github.com>
2024-07-26 12:26:49 -0400

    Implemented Score Widget
    -Checked “Simulation Generates Hit Event” on projectile, UFO, and UFO collision
    -Created WBP_Score Blueprint
    -Created IncreaseScore function in UserShip Blueprint
    -Expanded Event Graph - added connection from “Start AR Session” to “Create WBP Score Widget” and added that to Viewport.
    -Also added connection in BP_Projectile Event Graph - added connections off of “Play Sound at Location” to trigger score increase

--- Capstone_UserShip/Intermediate/Build/IOS/arm64/Capstone_UserShip/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
+++ Capstone_UserShip/Intermediate/Build/IOS/arm64/Capstone_UserShip/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "/Users/daniel/Documents/GitHub/CS467_Capstone_Project/Capstone_UserShip/Intermediate/Build/IOS/arm64/Capstone_UserShip/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
+#include "/Users/stephengallivan/Documents/GitHub/CS467_Capstone_Project/Capstone_UserShip/Intermediate/Build/IOS/arm64/Capstone_UserShip/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
 #include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
